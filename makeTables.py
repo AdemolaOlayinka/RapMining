849ca622fa4fbc757942e27239ff453635be24e3
Matt Luppino <luppino15@comcast.net>
2017-03-09 16:42:25 -0500

    Started parsing JSON

--- makeTables.py
+++ makeTables.py
@@ -1,17 +1,37 @@
 import sqlite3 as lite
+import json
 import sys
 
-con = lite.connect("RapMining.db")
+def makeDB():
+    con = lite.connect("RapMining.db")
+    with con:
+        cur = con.cursor()
+        cur.execute("DROP TABLE IF EXISTS Albums")
+        cur.execute("DROP TABLE IF EXISTS Songs")
+        cur.execute("DROP TABLE IF EXISTS Words")
+        cur.execute("CREATE TABLE Albums(ID INT, Name TEXT, Artist TEXT, Year INT)")
+        cur.execute("CREATE TABLE Songs(ID INT, Name TEXT, Album TEXT, Lyrics TEXT)")
+        cur.execute("CREATE TABLE Words(ID INT, Word TEXT, Freq INT)")  
 
-with con:
+def navigateDirectory():
+    directory = "../txt_files/"
+    for artist in os.listdir(directory):
+        dirArt = directory + artist + "/"
+        for album in os.listdir(dirArt):
+            dirAlb = dirArt + album + "/"
+            for song in os.listdir(dirAlb):
+                songFile = dirAlb+song
+                data = json.lead(songFile)
+                addValues(data, artist, album, song)
+                
+def addValues(data, artist, album, song):
+    total_words = data["total_words"][0]
+    total_dict = data["total_words"][1]
+    lyrics = data["lyrics"]
+    unique_words = data["unique_words"][0]
+    unique_dict = data["unique_words"][1]
     
-    cur = con.cursor()
-    cur.execute("DROP TABLE IF EXISTS Albums")
-    cur.execute("DROP TABLE IF EXISTS Songs")
-    cur.execute("DROP TABLE IF EXISTS Words")
-    cur.execute("CREATE TABLE Albums(ID INT, Name TEXT, Artist TEXT, Year INT)")
-    cur.execute("CREATE TABLE Songs(ID INT, Name TEXT, Album TEXT, Lyrics TEXT)")
-    cur.execute("CREATE TABLE Words(ID INT, Word TEXT, Freq INT)")
-    cur.execute("INSERT INTO Albums VALUES(1, 'The Marshall Mathers LP', 'Eminem', 2000)")
-    cur.execute("INSERT INTO Songs VALUES(1, 'The Real Slim Shady', 'The Marshall Mathers LP', 'Y''all act like...')")
+
+if __name__ == '__main__':
+    makeDB()
     
